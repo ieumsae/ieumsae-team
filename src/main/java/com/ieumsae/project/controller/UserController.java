397f8dd0292b921043690e938b859e53c6030048
Soo186 <rlagustn9797@gmail.com>
2024-07-01 17:56:41 +0900

    중복확인 버튼으로 중복확인 기능 구현에 필요한 메서드 호출 후 매핑 추가

--- src/main/java/com/ieumsae/project/controller/UserController.java
+++ src/main/java/com/ieumsae/project/controller/UserController.java
@@ -3,12 +3,15 @@ package com.ieumsae.project.controller;
 import com.ieumsae.project.domain.User;
 import com.ieumsae.project.domain.UserForm;
 import com.ieumsae.project.domain.UserInterest;
+import com.ieumsae.project.service.UserInterestService;
 import com.ieumsae.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -16,11 +19,13 @@ public class UserController {
 
 
     private final UserService userService;
+    private final UserInterestService userInterestService;
 
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserInterestService userInterestService) {
         this.userService = userService;
+        this.userInterestService = userInterestService;
     }
     @GetMapping("/signup")
     public String signup(Model model) {
@@ -48,17 +53,26 @@ public class UserController {
         user.setUserEmail(form.getUserEmail());
         user.setUserAddress(form.getUserAddress());
         user.setUserGender(form.getUserGender());
-
-        UserInterest userInterest = new UserInterest();
-        userInterest.setUserInterest1(form.getUserInterest1());
-        userInterest.setUserInterest1(form.getUserInterest2());
-        userInterest.setUserRegion1(form.getUserReigon1());
-        userInterest.setUserRegion1(form.getUserReigon2());
-
         // 3. UserService를 통해 새로운 사용자 등록
         userService.join(user);
 
-        // 4. 회원가입 완료 후 홈페이지로 리다이렉트
+
+        UserInterest userInterest = new UserInterest();
+        userInterest.setUserInterest1(form.getUserInterest1());
+        userInterest.setUserInterest2(form.getUserInterest2());
+        userInterest.setUserRegion1(form.getUserReigon1());
+        userInterest.setUserRegion2(form.getUserReigon2());
+
+        //4. UserInterestService를 통해 사용자 관심사 등록
+            userInterestService.save(userInterest);
+
+
+        // 5. 회원가입 완료 후 홈페이지로 리다이렉트
         return "redirect:/";
+    }
+    @PostMapping("/check-duplicate")
+    @ResponseBody
+    public boolean checkDuplicate(@RequestParam String field, @RequestParam String value) {
+        return userService.checkDuplicate(field, value);
     }
 }
