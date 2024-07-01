0ced1820240d214fa190697c9576b04731b273bb
Soo186 <rlagustn9797@gmail.com>
2024-07-01 13:17:36 +0900

    GradePointRepository

---
+++ src/main/java/com/ieumsae/project/repository/GradePointRepository.java
@@ -0,0 +1,7 @@
+package com.ieumsae.project.repository;
+
+import com.ieumsae.project.domain.GradePoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GradePointRepository extends JpaRepository<GradePoint, Long> {
+}
