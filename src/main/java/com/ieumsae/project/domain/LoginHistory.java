1dc6aa93a34d347622bab2b9b942b53bc254b180
Soo186 <rlagustn9797@gmail.com>
2024-07-01 13:10:06 +0900

    LoginHistory Entity Class

---
+++ src/main/java/com/ieumsae/project/domain/LoginHistory.java
@@ -0,0 +1,25 @@
+package com.ieumsae.project.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "login_history")
+public class LoginHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
+    @Column(name = "user_idx")
+    private int user_idx;
+
+    @Column(name = "login_dt")
+    private Date login_dt;
+}
