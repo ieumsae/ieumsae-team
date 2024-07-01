bdf1a99cfb966a498c65b9f84af3ac1113ef3457
Soo186 <rlagustn9797@gmail.com>
2024-07-01 13:18:31 +0900

    UserInterestRepository

---
+++ src/main/java/com/ieumsae/project/repository/UserInterestRepository.java
@@ -0,0 +1,8 @@
+package com.ieumsae.project.repository;
+
+import com.ieumsae.project.domain.UserInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
+
+}
