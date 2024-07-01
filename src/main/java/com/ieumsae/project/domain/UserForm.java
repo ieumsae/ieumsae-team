49dc9762afa8a23b7818fd87d73a7b329b2e891a
Soo186 <rlagustn9797@gmail.com>
2024-07-01 13:05:13 +0900

    UserForm Class

---
+++ src/main/java/com/ieumsae/project/domain/UserForm.java
@@ -0,0 +1,23 @@
+package com.ieumsae.project.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserForm {
+    private String userId;
+    private String userPw;
+    private String userNickName;
+    private Short userGender;
+    private String userMbti;
+    private String userName;
+    private String userPhone;
+    private String userEmail;
+    private String userInterest1;
+    private String userInterest2;
+    private String userReigon1;
+    private String userReigon2;
+    private String userAddress;
+}
