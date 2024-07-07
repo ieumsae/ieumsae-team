53a4270fbc0fda78533ba6a0fa9cfe7f89006af4
Soo186 <rlagustn9797@gmail.com>
2024-07-07 19:32:34 +0900

    관리자 컨트롤러

--- src/main/java/com/ieumsae/project/repository/UserInterestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ieumsae.project.repository;
-
-import com.ieumsae.project.domain.UserInterest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
-
-}
