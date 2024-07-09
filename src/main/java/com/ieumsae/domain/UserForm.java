55d7e4f2872ed06b0cf368419c5e455fd7674b07
Soo186 <rlagustn9797@gmail.com>
2024-07-09 23:46:59 +0900

    회원가임 폼

---
+++ src/main/java/com/ieumsae/domain/UserForm.java
@@ -0,0 +1,16 @@
+package com.ieumsae.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserForm {
+    private String userId;
+    private String userName;
+    private String userNickName;
+    private String userPw;
+    private String userEmail;
+
+}
