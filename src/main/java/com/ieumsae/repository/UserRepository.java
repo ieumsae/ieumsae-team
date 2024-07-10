82bec8985addbc96a19f2714a4eb41c38c3dc6db
Soo186 <rlagustn9797@gmail.com>
2024-07-10 09:39:41 +0900

    유저 저장소

---
+++ src/main/java/com/ieumsae/repository/UserRepository.java
@@ -0,0 +1,13 @@
+    package com.ieumsae.repository;
+
+    import com.ieumsae.domain.User;
+    import org.springframework.data.jpa.repository.JpaRepository;
+
+    import java.util.Optional;
+
+
+    public interface UserRepository extends JpaRepository<User, Long> {
+        Optional<User> findByUserId(String userId);
+        Optional<User> findByUserNickName(String userNickName);
+        Optional<User> findByUserEmail(String userEmail);
+    }
