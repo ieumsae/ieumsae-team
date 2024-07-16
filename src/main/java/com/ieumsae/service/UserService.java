6184a085b83db15c9214add97aa63523b17cc6b6
Soo186 <rlagustn9797@gmail.com>
2024-07-16 19:57:05 +0900

    유저 서비스 수정

--- src/main/java/com/ieumsae/service/UserService.java
+++ src/main/java/com/ieumsae/service/UserService.java
@@ -2,10 +2,17 @@ package com.ieumsae.service;
 
 import com.ieumsae.domain.User;
 import com.ieumsae.domain.UserForm;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserService {
-    User join(UserForm form);
-    User findById(Long id);
+    User findById(Long userIdx);
+
+    @Transactional
+    Long signUp1(UserForm form);
+
+    @Transactional
+    User signUp2(Long userIdx, String nickname);
+
     boolean checkDuplicate(String field, String value);
 
 
