d1bedd071a6731f32d6cc658b44fbbd621d5fb56
Soo186 <rlagustn9797@gmail.com>
2024-07-17 20:03:47 +0900

    유저 폼에 식별자 추가

--- src/main/java/com/ieumsae/domain/UserForm.java
+++ src/main/java/com/ieumsae/domain/UserForm.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserForm {
+    private Long userIdx;
     private String userId;
     private String userName;
     private String userNickName;
