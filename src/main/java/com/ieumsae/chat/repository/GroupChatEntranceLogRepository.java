3a74363ae7adcb7472e15fda6f42af9743fe15c5
pangwoo96 <junggw96@naver.com>
2024-07-22 20:07:00 +0900

    JPA 메소드 수정 -> 1:1 채팅과 같은 형태로 변경

--- src/main/java/com/ieumsae/chat/repository/GroupChatEntranceLogRepository.java
+++ src/main/java/com/ieumsae/chat/repository/GroupChatEntranceLogRepository.java
@@ -8,6 +8,10 @@ import java.util.Optional;
 
 @Repository
 public interface GroupChatEntranceLogRepository extends JpaRepository<GroupChatEntranceLog, Long> {
+
+    // 이전에 접속 기록이 있는지 확인하는 메소드
+    boolean existsByChatIdxAndUserIdx(Integer chatIdx, Integer userIdx);
+
     // 특정 그룹 채팅방에 특정 사용자가 가장 최근에 입장한 기록을 조회
-    Optional<GroupChatEntranceLog> findTopByGroupChatIdxAndUserIdxOrderByEntranceDateTimeDesc(Integer groupChatIdx, Integer userIdx);
+    Optional<GroupChatEntranceLog> findFirstByChatIdxAndUserIdxOrderByEntranceDateTimeDesc(Integer groupChatIdx, Integer userIdx);
 }
