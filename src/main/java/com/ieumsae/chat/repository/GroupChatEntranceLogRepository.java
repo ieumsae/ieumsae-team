dd099a880959d682b7c3cf0576f23e4e5385164b
pangwoo96 <junggw96@naver.com>
2024-07-22 20:43:38 +0900

    채팅 입장기록이 있는지 확인하는 JPA 메소드 추가

--- src/main/java/com/ieumsae/chat/repository/GroupChatEntranceLogRepository.java
+++ src/main/java/com/ieumsae/chat/repository/GroupChatEntranceLogRepository.java
@@ -1,5 +1,6 @@
 package com.ieumsae.chat.repository;
 
+import com.ieumsae.chat.domain.ChatEntranceLog;
 import com.ieumsae.chat.domain.GroupChatEntranceLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,6 @@ public interface GroupChatEntranceLogRepository extends JpaRepository<GroupChatE
 
     // 특정 그룹 채팅방에 특정 사용자가 가장 최근에 입장한 기록을 조회
     Optional<GroupChatEntranceLog> findFirstByChatIdxAndUserIdxOrderByEntranceDateTimeDesc(Integer groupChatIdx, Integer userIdx);
+
+    Optional<GroupChatEntranceLog> findByChatIdxAndUserIdx(Integer chatIdx, Integer userIdx);
 }
