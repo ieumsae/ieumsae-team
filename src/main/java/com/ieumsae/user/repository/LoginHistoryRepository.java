fa2b64bf31338425f20204139e95018934f84d42
Soo186 <rlagustn9797@gmail.com>
2024-08-14 12:24:44 +0900

    회원등급 클래스 삭제

--- src/main/java/com/ieumsae/user/repository/LoginHistoryRepository.java
+++ src/main/java/com/ieumsae/user/repository/LoginHistoryRepository.java
@@ -1,6 +1,5 @@
 package com.ieumsae.user.repository;
 
-import com.ieumsae.user.domain.LoginHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginHistoryRepository extends JpaRepository<LoginHistory, Long> {
