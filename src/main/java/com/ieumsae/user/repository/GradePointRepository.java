82eb1bbbe08a8fcac373c02f0e49d0b9335e79f4
pangwoo96 <junggw96@naver.com>
2024-08-16 16:09:04 +0900

    user 패키지 안의 repo 삭제

--- src/main/java/com/ieumsae/user/repository/GradePointRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ieumsae.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradePointRepository extends JpaRepository<GradePoint, Long> {
-}
